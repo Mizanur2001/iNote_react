4189455d0edbf86a2de44f501641d74bc5c6318d
Mizanur <jameslev2001@gmail.com>
2022-02-06 00:15:09 +0530

    add some validation and we use express-validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res) => {
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-});
+
+router.post('/createuser', [
+    body('email', 'Enter a valid Email').isEmail(),
+    body('name', 'Name shuld be more than 4 char').isLength({ min: 4 }),
+    body('password', 'enter a strong Password password shuld be more than 8 char').isLength({ min: 8 })], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res.status(400).json({ error: `Sorry ${req.body.email} alrady exist` });
+            }
+            user = await User.create({
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email
+            })
+            res.json({ Success: `Hey ${req.body.name} ! you Accunt created Successfully`, user });
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Some error occyred");
+        }
+    });
 
 module.exports = router;
