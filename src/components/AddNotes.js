00f26deb333636cc9b9abbb3b9bbb9f512841b04
Mizanur <jameslev2001@gmail.com>
2022-02-09 23:21:20 +0530

    Added a disable button if char length is less than 5

--- src/components/AddNotes.js
+++ src/components/AddNotes.js
@@ -5,31 +5,35 @@ import { useContext, useState } from 'react';
 function AddNotes() {
     const context = useContext(noteContext);
     const { funcAddNote } = context;
-    const [notesdata, setNotesdata] = useState({ title: "", description: "", tag: "Default" });
+    const [notesdata, setNotesdata] = useState({ title: "", description: "", tag: "" });
+
     const funcSubmitNote = (e) => {
         e.preventDefault();
         funcAddNote(notesdata);
+        setNotesdata({ title: "", description: "", tag: "" });
     }
+
     const funcOnChange = (e) => {
         setNotesdata({ ...notesdata, [e.target.name]: e.target.value });
     }
+
     return (
         <div className="container">
             <h2 className='my-3'>Add Notes</h2>
             <form>
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Enter Title</label>
-                    <input type="text" className="form-control" id="title" placeholder="Your Title length should be more than 3 char" name='title' onChange={funcOnChange} />
+                    <input type="text" className="form-control" id="title" placeholder="Your Title length should be more than 5 char" name='title' onChange={funcOnChange} value={notesdata.title} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="tag" className="form-label">Tag</label>
-                    <input type="text" className="form-control" id="tag" placeholder="Enter tag here" name='tag' onChange={funcOnChange} />
+                    <input type="text" className="form-control" id="tag" placeholder="Enter tag here" name='tag' onChange={funcOnChange} value={notesdata.tag} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="description" className="form-label">Enter Description</label>
-                    <textarea className="form-control" id="description" name='description' rows="3" onChange={funcOnChange} />
+                    <textarea className="form-control" id="description" name='description' rows="3" onChange={funcOnChange} value={notesdata.description} />
                 </div>
-                <button type="submit" className="btn btn-primary" onClick={funcSubmitNote}>Submit</button>
+                <button disabled={notesdata.title.length < 5 || notesdata.description.length < 5} type="submit" className="btn btn-primary" onClick={funcSubmitNote}>Submit</button>
             </form>
         </div>
     )
