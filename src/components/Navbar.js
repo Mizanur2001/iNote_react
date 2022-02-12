68c483cb3ed5b92128c9437c8dbe84316c550adb
Mizanur <jameslev2001@gmail.com>
2022-02-12 22:42:07 +0530

    added user name at navbar from database

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
@@ -7,9 +7,27 @@ function Navbar() {
     }
     let location = useLocation();
     useEffect(() => { }, [location]);
+
+    //Geting User Data
+    const [user, setUser] = useState('blank');
+    const funcGetUser = async () => {
+        const responce = await fetch('http://localhost:5000/api/auth/getuser', {
+            method: 'POST',
+            headers: {
+                'authToken': localStorage.getItem('authToken'),
+                'Content-Type': 'application/json'
+            }
+        });
+        const jsonData = await responce.json();
+        setUser(jsonData.name);
+    }
+
+    if (localStorage.getItem('authToken')) {
+        funcGetUser()
+    }
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">iNotes</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +45,7 @@ function Navbar() {
                         {!localStorage.getItem('authToken') ? <form className="d-flex">
                             <Link className="btn btn-outline-primary mx-1" to="/login" role="button">Login</Link>
                             <Link className="btn btn-outline-primary mx-1" to="/signup" role="button">SignUp</Link>
-                        </form> : <Link className="btn btn-outline-warning mx-1" to="/login" onClick={funcLogout} role="button">Logout</Link>}
+                        </form> : <div className="d-flex" style={{ alignItems: 'center' }}><h5 style={{ margin: '0px 8px' }} className='text-light'>{`Welcome ${user} !`}</h5> <Link className="btn btn-outline-warning mx-1" to="/login" onClick={funcLogout} role="button">Logout</Link></div>}
                     </div>
                 </div>
             </nav>
